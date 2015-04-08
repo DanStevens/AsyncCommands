274b00596e6d140c6acf0f909ff863e11a530c50
Dan Stevens <dan.stevens@oracle.com>
2015-04-08 16:56:39 +0100

    Made a change to the DownloadAndCountBytesAsync method of the Service class that results in an exception being throw before first await call if the url argument is null or empty. This results in an unhandled exception, instead of the NotifyTaskCompletion class swallowing the exception and providing through its Exception property.

--- AsyncCommands3/Service.cs
+++ AsyncCommands3/Service.cs
@@ -8,6 +8,9 @@ public static class MyService
     // http://blogs.msdn.com/b/lucian/archive/2012/12/08/await-httpclient-getstringasync-and-cancellation.aspx
     public static async Task<int> DownloadAndCountBytesAsync(string url, CancellationToken token = new CancellationToken())
     {
+        if (String.IsNullOrWhiteSpace(url))
+            throw new ArgumentNullException("url");
+        
         await Task.Delay(TimeSpan.FromSeconds(3), token).ConfigureAwait(false);
         var client = new HttpClient();
         using (var response = await client.GetAsync(url, token).ConfigureAwait(false))
